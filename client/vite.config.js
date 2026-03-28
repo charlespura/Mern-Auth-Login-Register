ef060780de7f4da8976afadb52adb21facdc2120
Charles Pura <164540352+charlespura@users.noreply.github.com>
2026-03-28 17:05:46 +0800

    up up add some locat

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mern-Auth-Login-Register/'
+  base: '/Mern-Auth-Login-Register/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 })
